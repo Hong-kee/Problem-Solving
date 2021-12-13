3379da7b753fa0c2f5532fbad2b834c5cf7d650d
Hong-kee <ghdehfdl123@gmail.com>
2021-12-13 23:58:03 +0900

    BaekJoon11728 배열 합치기

---
+++ AlgorithmProject/BaekJoon11728.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+	int num, N, M; cin >> N >> M;
+	priority_queue<int, vector<int>, greater<int>> pq;
+
+	for (int i = 0; i < N; i++) {
+		cin >> num; pq.push(num);
+	}
+	for (int i = 0; i < M; i++) {
+		cin >> num; pq.push(num);
+	}
+	while (!pq.empty()) { cout << pq.top() << ' '; pq.pop(); }
+}
